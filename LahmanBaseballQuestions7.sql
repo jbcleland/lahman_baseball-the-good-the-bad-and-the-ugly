0fc3ed6a747e27b7db5d5c3f3c3798245f3d3c32
jbcleland <bcleland@gmail.com>
2022-02-17 09:52:49 -0600

    Final Update

--- LahmanBaseballQuestions7.sql
+++ LahmanBaseballQuestions7.sql
@@ -36,14 +36,14 @@ limit 1;
 
 	--max winners
 		SELECT distinct yearid, teamid,
-			--max(W) OVER(partition by yearid) as highest_wins
-			max(W)
+			max(W) OVER(partition by yearid) as highest_wins
+			--max(W)
 		FROM teams
 		WHERE yearid between 1970 and 2016
-		and ws_win = 'Y'
+		--and ws_win = 'Y'
 		--AND higest_wins 
 		--GROUP BY yearid, teamid, name, wswin
-		GROUP BY yearid, teamid
+		--GROUP BY yearid, teamid
 		order by yearid;
 	
 	--world series winners
@@ -87,11 +87,15 @@ group by  (yearid, teamid )
 order by max desc
 limit 46;
 
-SELECT teamid, yearid, w
-FROM teams
-WHERE wswin = 'Y'
-AND yearid between 1970 and 2016
-ORDER BY yearid;
+WITH ws_Winners as (SELECT teamid, yearid, w
+					FROM teams
+					WHERE wswin = 'Y'
+					AND yearid between 1970 and 2016
+					ORDER BY yearid)
+SELECT max(win) as big_winners
+FROM teams full join ws_Winners 
+ON teams.year.id=ws_Winners.year_id
+WHERE ws_winners = big_winners;
 
 
 SELECT teamid, yearid, w, wswin
@@ -99,6 +103,28 @@ FROM teams
 WHERE yearid between 1970 and 2016
 GROUP BY teamid, yearid, wswin,w
 ORDER BY yearid;
+
+--How often from 1970 – 2016 was it the case that a team with the most wins also won the world series?
+
+WITH big_winners as (SELECT yearid, teamid, w, wswin,
+		        RANK() OVER (PARTITION BY yearid ) as win_rank, max(w) over (PARTITION BY yearid) as max_wins
+				FROM teams
+				WHERE yearid between 1970 and 2016
+				--and ws_win = 'Y'
+				--AND higest_wins 
+				--GROUP BY yearid, teamid, name, wswin
+				GROUP BY yearid, teamid, w, wswin
+				order by yearid, w desc)
+SELECT (ROUND(count(*),1)/46)*100
+--yearid, teamid, max_wins
+from big_winners
+--WHERE yearid between 1970 and 2016
+WHERE wswin = 'Y'
+AND W=max_wins
+--group by  (yearid, teamid,max_wins)
+
+
+
 	
 
 		--What percentage of the time?
